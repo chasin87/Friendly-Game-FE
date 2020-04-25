0c455be30fb81b416155500c07daa6a2bb792497
chasin87 <yasinyuksek87@gmail.com>
2020-04-26 00:12:41 +0200

    add HeaderImg

---
+++ src/pages/Home/index.js
@@ -0,0 +1,10 @@
+import React from "react";
+import headerImg from "../../images/headerImg.png";
+
+export default function Home() {
+  return (
+    <div>
+      <img src={headerImg} alt="logo" style={{ width: "100%" }} />
+    </div>
+  );
+}
