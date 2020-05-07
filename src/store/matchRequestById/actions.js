4daec3126a17cfdd96ce26c528e462ce66e50bfd
chasin87 <yasinyuksek87@gmail.com>
2020-05-07 13:42:26 +0200

    fetched home and away games for team

--- src/store/matchRequestById/actions.js
+++ src/store/matchRequestById/actions.js
@@ -10,20 +10,8 @@ export const fetchRequest = (request) => ({
 
 export function fetchRequestList(userIdHome, userIdAway) {
   return async (dispatch, getState) => {
-    const requestOne = axios.get(`${apiUrl}/matchrequests/${userIdHome}`);
-    const requestTwo = axios.get(`${apiUrl}/matchrequests/${userIdAway}`);
-
-    axios.all([requestOne, requestTwo]).then(
-      axios.spread((...responses) => {
-        const responseOne = responses[0];
-        const responseTwo = responses[1];
-        const totalResponse = (responseOne.data, responseTwo.data);
-        console.log("res", totalResponse);
-        dispatch(fetchRequest(totalResponse));
-      })
-    );
+    const response = await axios.get(`${apiUrl}/matchrequests/${userIdHome}`);
+    console.log("res", response);
+    dispatch(fetchRequest(response.data));
   };
 }
-// const response = await axios.get(`${apiUrl}/matchrequests/${userIdHome}`);
-// console.log("res", response);
-//
